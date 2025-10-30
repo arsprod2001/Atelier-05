f6adb1ecb8a2f3815644729e668a647b6a08da2a
arsprod2001 <sow.amadou-ibrahima@ugb.edu.sn>
2025-10-29 23:47:22 -0400

    Exo 2 : Implementation Format Exception

---
+++ Exercice 02/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  try {
+    print("Entrez votre age :");
+    String? input = stdin.readLineSync();
+    int age = int.parse(input!);
+    print("Votre age est $age ans");
+  } on FormatException catch (e) {
+    print("Format age invalide");
+  } 
+}
